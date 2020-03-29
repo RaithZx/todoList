cb33f58fcbf6a1bdb6838b4a32a71ca546f3373c
Yuri Leandro <y.leandrof@gmail.com>
2020-03-29 21:05:37 +0200

    Initial page

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,19 +1,62 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 
 const App = () => {
-  const [name , setName] = useState('Yuri');
-  const onNameChange = useCallback((event) => {
-    console.log(event.target.value)
-    setName(event.target.value)
+  const [newTodo , setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const onNewTodoChange = useCallback((event) => {
+    setNewTodo(event.target.value)
   },[])
+  
+  const formSubmitted = useCallback((event) =>{
+    event.preventDefault();
+    if (!newTodo.trim()) return;
+    setTodos([
+      ...todos,
+      {
+        id:todos.length+1,
+        content: newTodo,
+        done: false
+      }
+    ])
+    setNewTodo('')
+  }, [newTodo, todos]);
+  
+  const addTodo = (todo, index) => (event) => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1, {
+      ...todo,
+      done: !todo.target.checked
+    })
+  }
+  useEffect(() => {
+    console.log('todos', todos)
+  },[todos])
+
   return (
     <div>
-      <form>
-        <label>Enter your name:</label>
-        <input value={name} onChange={onNameChange}/>
+      <form onSubmit={formSubmitted}>
+        <label for="newTodo">Enter a todo:</label>
+        <input 
+          id="newTodo"
+          name="newTodo"
+          value={newTodo}
+          onChange={onNewTodoChange}
+          />
+          <button>Add todo</button>
       </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox"
+              checked={todo.done}
+              onChange={addTodo}
+            />
+            {todo.content}
+            </li>
+        ))}
+      </ul>
       <div>
-        <h1>Hello {name}</h1>
       </div>
     </div>
   );
